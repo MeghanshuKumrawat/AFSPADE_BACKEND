9a09eccef6cef744434c7a352fbe6a53e20a3954
MeghanshuKumrawat <meghanshukumrawat321@gmail.com>
2024-10-04 12:45:29 +0530

    added file and remove questions

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from main.models import Course, Assignment, Question, CourseEnrollment, Submission
+from main.models import Course, Assignment, CourseEnrollment, Submission
 
 admin.site.register(Course)
 admin.site.register(Assignment)
-admin.site.register(Question)
+# admin.site.register(Question)
 admin.site.register(CourseEnrollment)
 admin.site.register(Submission)
 
--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,11 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path
-from .views import CourseViewSet, AssignmentViewSet, QuestionViewSet, CourseEnrollmentViewSet, SubmissionViewSet, DashboardView
+from .views import CourseViewSet, AssignmentViewSet, CourseEnrollmentViewSet, SubmissionViewSet, DashboardView
 
 router = DefaultRouter(trailing_slash=False)
 router.register(r'courses', CourseViewSet, basename='course')
 router.register(r'assignments', AssignmentViewSet, basename='assignments')
-router.register(r'questions', QuestionViewSet, basename='questions')
+# router.register(r'questions', QuestionViewSet, basename='questions')
 router.register(r'enrollments', CourseEnrollmentViewSet, basename='course-enrollment')
 router.register(r'submissions', SubmissionViewSet, basename='submission')
 
